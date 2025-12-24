6e8637edf50d285368ba99d2042cad6abdc6f4a8
Divyanshu Dugar <divyu2004@gmail.com>
2025-12-24 10:40:32 -0500

    added code snippets for list comprehension

---
+++ Udemy/05_comprehensions/list.py
@@ -0,0 +1,11 @@
+menu = [
+    "Masala Chai",
+    "Iced lemon Tea",
+    "Green Tea",
+    "Iced Peach Tea",
+    "Ginger Chai"
+]
+
+iced_tea = [tea for tea in menu if "Iced" in tea]
+
+print(iced_tea)
